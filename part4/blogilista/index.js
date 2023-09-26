05e27bf6208e8dfafabf584be15fdd2543aec9a0
Janne Saari <jannesaari24@gmail.com>
2023-09-26 17:19:03 +0300

    Separate blog model and controller to their own files

--- part4/blogilista/index.js
+++ part4/blogilista/index.js
@@ -5,48 +5,30 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
 
 mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)
-.then(result => {
-    logger.info(result)
-    logger.info('Connected to MongoBD')
-  })
-  .catch((error) => {
+    .then(result => {
+        logger.info(result)
+        logger.info('Connected to MongoBD')
+    })
+    .catch((error) => {
     logger.error(`error connecting to MongoBD: ${error}`)
-  })
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+    })
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
+// app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.use('/api/blogs', blogsRouter)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+// app.use(middleware.unknownEndpoint)
+// app.use(middleware.errorHandler)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
