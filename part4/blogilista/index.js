cf9276de541cabc34debd0525ddf3e1be0183528
Janne Saari <jannesaari24@gmail.com>
2023-09-27 14:15:39 +0300

    Create middleware file

--- part4/blogilista/index.js
+++ part4/blogilista/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
+const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 
 mongoose.set('strictQuery', false)
@@ -12,7 +13,7 @@ mongoose.set('strictQuery', false)
 logger.info('connecting to', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)
     .then(result => {
-        logger.info(result)
+        // logger.info(result)
         logger.info('Connected to MongoBD')
     })
     .catch((error) => {
@@ -22,13 +23,12 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-// app.use(middleware.requestLogger)
-
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 
-// app.use(middleware.unknownEndpoint)
-// app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
