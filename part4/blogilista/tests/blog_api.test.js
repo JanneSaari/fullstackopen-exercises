7461e3ec598e3cc95c84558966cfd0a270ff0cbd
Janne Saari <jannesaari24@gmail.com>
2023-10-10 16:14:00 +0300

    Finish 4.9, change _id to id in schema and create testing for it

--- part4/blogilista/tests/blog_api.test.js
+++ part4/blogilista/tests/blog_api.test.js
@@ -9,6 +9,11 @@ test('blogs are returned as json', async () => {
   await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
 })
 
+test('identifying field is called: id', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
