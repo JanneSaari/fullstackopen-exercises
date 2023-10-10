9e0fdcce4feeea65d51a5ce419392e45d13cdf30
Janne Saari <jannesaari24@gmail.com>
2023-10-10 16:47:28 +0300

    Finish 4.10, add test for POSTing new blog

--- part4/blogilista/tests/blog_api.test.js
+++ part4/blogilista/tests/blog_api.test.js
@@ -14,6 +14,26 @@ test('identifying field is called: id', async () => {
   expect(response.body[0].id).toBeDefined()
 })
 
+test('new blog can be added', async () => {
+  const first = await api.get('/api/blogs')
+  const countBefore = first.body.length
+
+  const newBlog = {
+    'title': 'TestTitle',
+    'author': 'TestName',
+    'url': 'some/test/url',
+    'likes': '5'
+  }
+
+  await api.post('/api/blogs').send(newBlog)
+
+  const second = await api.get('/api/blogs')
+  const countAfter = second.body.length
+
+  expect(countAfter === countBefore + 1)
+  console.log(countBefore, countAfter)
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
