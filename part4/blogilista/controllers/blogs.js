f3cafe39bf1b85c3d863c342bbd097121ad40927
Janne Saari <jannesaari24@gmail.com>
2023-10-16 14:25:23 +0300

    Change blog POST to async await

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const { request, response } = require('../app')
 const Blog = require('../models/blog')
 
 const logger = require('../utils/logger')
@@ -6,23 +7,20 @@ const logger = require('../utils/logger')
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
   response.json(blogs)
-  // .then(blogs => {
-  //   response.json(blogs)
-  // })
-  // .catch(error => next(error))
 })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
   logger.info(blog)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => next(error))
+  const result = await blog.save()
+  response.status(201).json(result)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204)
 })
 
 module.exports = blogsRouter
