bbe190e4dbcfae9cf6badfe24a61f2bd8a8de2b6
Janne Saari <jannesaari24@gmail.com>
2023-11-20 16:02:05 +0200

    Add linting

--- part4/bloglist-frontend/src/App.jsx
+++ part4/bloglist-frontend/src/App.jsx
@@ -8,8 +8,8 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('') 
-  const [password, setPassword] = useState('') 
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [notification, setNotification] = useState(null)
   const [isNotificationError, setIsNotificationError] = useState(false)
@@ -17,7 +17,7 @@ const App = () => {
   const blogFormRef = useRef()
 
   useEffect(() => {
-    blogService.getAll().then(blogs => 
+    blogService.getAll().then(blogs =>
       setBlogs(blogs)
     )
   }, [])
@@ -88,23 +88,23 @@ const App = () => {
   }
 
   const updateBlog = async (updatedBlog) => {
-    const newBlog = {...updatedBlog}
+    const newBlog = { ...updatedBlog }
     console.log(newBlog)
     await blogService.updateBlog(newBlog)
 
     setNotification(`Blog "${newBlog.title}" by ${newBlog.author} updated`)
     setIsNotificationError(false)
-    
+
     const foo = await blogService.getAll()
     setBlogs(foo)
   }
 
   const deleteBlog = async (blog) => {
     await blogService.deleteBlog(blog)
-    
+
     setNotification(`Blog "${blog.title}" by ${blog.author} deleted`)
     setIsNotificationError(true)
-    
+
     const foo = await blogService.getAll()
     setBlogs(foo)
     // await updateBlogList()
@@ -121,24 +121,24 @@ const App = () => {
       <form onSubmit={handleLogin}>
         <div>
           username
-            <input
+          <input
             type="text"
             value={username}
             name="Username"
             onChange={({ target }) => setUsername(target.value)}
-            />
+          />
         </div>
         <div>
           password
-            <input
+          <input
             type="password"
             value={password}
             name="Password"
             onChange={({ target }) => setPassword(target.value)}
-            />
+          />
         </div>
         <button type="submit">login</button>
-      </form>      
+      </form>
     </div>
   )
 
@@ -147,16 +147,16 @@ const App = () => {
       <h2>blogs</h2>
       {console.log(user)}
       {blogs
-      .sort((a, b) => {
-        console.log('sorting', a, b)
-        const result = a.likes <= b.likes
-        console.log(result)
-        return result
-      })
-      .map(blog =>
-        <Blog key={blog.id} blog={blog} updateBlogFn={updateBlog} deleteBlogFn={deleteBlog} />
+        .sort((a, b) => {
+          console.log('sorting', a, b)
+          const result = a.likes <= b.likes
+          console.log(result)
+          return result
+        })
+        .map(blog =>
+          <Blog key={blog.id} blog={blog} updateBlogFn={updateBlog} deleteBlogFn={deleteBlog} />
         )
-        }
+      }
     </div>
   )
 
@@ -164,10 +164,10 @@ const App = () => {
     <div>
       <Notification message={notification} isError={isNotificationError}/>
       {!user && loginForm()}
-      {user && 
+      {user &&
         <p>{user.username} has logged in <button type='button' onClick={handleLogout}>Logout</button></p>
       }
-      {user && 
+      {user &&
         <Togglable buttonLabel='new blog' cancelLabel='show less' ref={blogFormRef}>
           <BlogForm createBlog={createBlog} />
         </Togglable>}
