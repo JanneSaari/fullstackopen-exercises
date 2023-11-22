019afcdd5085403786868a453391217d92d3de43
Janne Saari <jannesaari24@gmail.com>
2023-11-22 16:09:00 +0200

    Add cypress with simple test, finish 5.17

---
+++ part4/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -0,0 +1,16 @@
+describe('Blog App', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Test User',
+      username: 'root',
+      password: 'sekret'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
+    cy.visit('http://localhost:5173')
+  })
+
+  it('Login form is shown', function() {
+    cy.contains('Login')
+  })
+})
