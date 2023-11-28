920a0c4b2b71fa24a33503d7775ed3350f177b55
Janne Saari <jannesaari24@gmail.com>
2023-11-28 16:42:34 +0200

    Add testing for sorting by likes, finish 5.23

--- part4/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part4/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -39,42 +39,46 @@ describe('Blog App', function() {
 
   describe('When logged in', function() {
     beforeEach( function() {
-      cy.request('POST', 'http://localhost:3001/api/login', {
+      const response = cy.request('POST', 'http://localhost:3001/api/login', {
         username: 'root',
         password: 'sekret'
-      })
-        .then(response => {
-          localStorage.setItem('loggedAppUser',
-            JSON.stringify(response.body))
-          cy.visit('http://localhost:5173')
+      }).then( response => {
+        localStorage.setItem('loggedAppUser',
+          JSON.stringify(response.body))
+
+        let testBlog = {
+          title: 'testTitle',
+          author: 'testAuthor',
+          url: 'some/test/url',
+          likes: 0
+        }
+        const token = JSON.parse(localStorage.getItem('loggedAppUser')).token
+        console.log('token: ', token)
+        //Read about cypress auth option in request https://docs.cypress.io/api/commands/request
+        cy.request({
+          method: 'POST',
+          url: 'http://localhost:3001/api/blogs',
+          body: testBlog,
+          auth : {
+            'bearer' : token
+          }
         })
+        cy.visit('http://localhost:5173')
+      })
     })
-
-    const addTestBlog = () => {
-      cy.contains('new blog').click()
-
-      cy.get('#title-field').type('testTitle')
-      cy.get('#author-field').type('testAuthor')
-      cy.get('#url-field').type('testURL')
-      cy.get('#add-blog-btn').click()
-
-      cy.contains('"testTitle" by testAuthor')
-    }
 
     it('Blog can be created', function() {
       cy.contains('new blog').click()
 
-      cy.get('#title-field').type('testTitle')
-      cy.get('#author-field').type('testAuthor')
-      cy.get('#url-field').type('testURL')
+      cy.get('#title-field').type('newBlog')
+      cy.get('#author-field').type('newAuthor')
+      cy.get('#url-field').type('newURL')
       cy.get('#add-blog-btn').click()
 
-      cy.contains('"testTitle" by testAuthor')
+      cy.contains('"newBlog" by newAuthor')
     })
 
     it('Blog can be liked', function() {
-      addTestBlog()
-
       cy.get('.blog-element').as('blog').contains('"testTitle" by testAuthor').click()
 
       cy.get('@blog').get('.likes-element').should('contain', 'Likes: 0')
@@ -85,8 +89,6 @@ describe('Blog App', function() {
     })
 
     it('User can delete blogs they have added', function() {
-      addTestBlog()
-
       cy.get('.blog-element').as('blog').contains('"testTitle" by testAuthor').click()
       cy.get('@blog').contains('Delete blog').click()
 
@@ -95,7 +97,6 @@ describe('Blog App', function() {
 
     describe('With multiple users', function() {
       it('User only sees delete button on blogs they have added', function() {
-        addTestBlog()
         localStorage.clear()
 
         cy.request('POST', 'http://localhost:3001/api/login', {
@@ -110,6 +111,67 @@ describe('Blog App', function() {
 
         cy.get('.blog-element').as('blog').contains('"testTitle" by testAuthor').click()
         cy.get('@blog').should('not.contain', 'Delete blog')
+      })
+    })
+
+    describe('With multiple blogs already added', function() {
+      beforeEach( function() {
+        const token = JSON.parse(localStorage.getItem('loggedAppUser')).token
+
+        //Read about cypress auth option in request https://docs.cypress.io/api/commands/request
+        let testBlog1 = {
+          title: 'ShouldBeFirst',
+          author: 'testAuthor',
+          url: 'some/test/url',
+          likes: 10
+        }
+        cy.request({
+          method: 'POST',
+          url: 'http://localhost:3001/api/blogs',
+          body: testBlog1,
+          auth : {
+            'bearer' : token
+          }
+        })
+
+        let testBlog3 = {
+          title: 'ShouldBeThird',
+          author: 'testAuthor',
+          url: 'some/test/url',
+          likes: 2
+        }
+        cy.request({
+          method: 'POST',
+          url: 'http://localhost:3001/api/blogs',
+          body: testBlog3,
+          auth : {
+            'bearer' : token
+          }
+        })
+
+        let testBlog2 = {
+          title: 'ShouldBeSecond',
+          author: 'testAuthor',
+          url: 'some/test/url',
+          likes: 5
+        }
+        cy.request({
+          method: 'POST',
+          url: 'http://localhost:3001/api/blogs',
+          body: testBlog2,
+          auth : {
+            'bearer' : token
+          }
+        })
+
+        cy.visit('http://localhost:5173')
+      })
+
+      it('Blogs are sorted by most likes', function() {
+        cy.get('.blog-element').eq(0).should('contain', 'ShouldBeFirst')
+        cy.get('.blog-element').eq(1).should('contain', 'ShouldBeSecond')
+        cy.get('.blog-element').eq(2).should('contain', 'ShouldBeThird')
+        cy.get('.blog-element').eq(3).should('contain', 'testTitle')
       })
     })
   })
