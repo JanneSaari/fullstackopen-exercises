464c36bdab4ef83c85c91daa52d29260d81c5aaf
Janne Saari <jannesaari24@gmail.com>
2023-11-29 15:18:41 +0200

    Finish 6.2

--- part4/unicafe-redux/src/reducer.js
+++ part4/unicafe-redux/src/reducer.js
@@ -6,15 +6,23 @@ const initialState = {
 
 const counterReducer = (state = initialState, action) => {
   console.log(action)
+  let newState = { ...state }
+  console.log(newState)
   switch (action.type) {
     case 'GOOD':
-      return state
+      newState.good += 1
+      return newState
     case 'OK':
-      return state
+      newState.ok += 1
+      return newState
     case 'BAD':
-      return state
+      newState.bad += 1
+      return newState
     case 'ZERO':
-      return state
+      newState.good = 0
+      newState.ok = 0
+      newState.bad = 0
+      return newState
     default: return state
   }
   
