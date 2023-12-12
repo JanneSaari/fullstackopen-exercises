eb5b77c53a926bf72b6242ad32bda5e333fe46c7
Janne Saari <jannesaari24@gmail.com>
2023-12-12 15:22:29 +0200

    Add filtering for anecdotes, finish 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,8 @@ import { useSelector, useDispatch } from 'react-redux'
 import { addVoteFor } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.anecdote)
+  const filter = useSelector(state => state.filter)
   const dispatch = useDispatch()
 
   const vote = (id) => {
@@ -12,19 +13,22 @@ const AnecdoteList = () => {
 
   return (
     <div>
-      {anecdotes
-        .toSorted((a, b) => a.votes < b.votes )
-        .map(anecdote =>
-          <div key={anecdote.id}>
-            <div>
-              {anecdote.content}
+      <div>
+        {anecdotes
+          .filter((anecdote) => anecdote.content.toLowerCase().includes(filter.toLowerCase()))
+          .toSorted((a, b) => a.votes < b.votes )
+          .map(anecdote =>
+            <div key={anecdote.id}>
+              <div>
+                {anecdote.content}
+              </div>
+              <div>
+                has {anecdote.votes}
+                <button onClick={() => vote(anecdote.id)}>vote</button>
+              </div>
             </div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => vote(anecdote.id)}>vote</button>
-            </div>
-          </div>
         )}
+      </div>
     </div>
   )
 }
