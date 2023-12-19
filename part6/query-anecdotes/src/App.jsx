1a6a34fa27e70549af710e8cd4f0d214966adc15
Janne Saari <jannesaari24@gmail.com>
2023-12-19 15:50:18 +0200

    Get anecdotes from json-server backend, finish 6.20

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,19 +1,27 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from './requests'
+
 const App = () => {
+
+  const result = useQuery({    
+    queryKey: ['anecdotes'],    
+    queryFn: () => getAnecdotes
+  })  
+  console.log('result: ', JSON.parse(JSON.stringify(result)))
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+  if ( result.isError ) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+  const anecdotes = result.data
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
-
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
 
   return (
     <div>
