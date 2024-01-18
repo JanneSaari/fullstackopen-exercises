59e175e9dbc15b39123aa4059f8b6ff0ee1854a6
Janne Saari <jannesaari24@gmail.com>
2024-01-18 13:43:30 +0200

    Change notifications to use Material UI

--- part4/bloglist-frontend/src/components/Notification.jsx
+++ part4/bloglist-frontend/src/components/Notification.jsx
@@ -1,37 +1,16 @@
 import { useSelector } from "react-redux"
+import Alert from '@mui/material/Alert'
 
 const Notification = () => {
   const notification = useSelector(state => state.notification)
 
-  const notificationStyle = {
-    color: "green",
-    background: "lightgrey",
-    fontSize: 20,
-    borderStyle: "solid",
-    borderSadius: 5,
-    padding: 10,
-    marginBottom: 10,
-    visibility: notification ? 'visible' : 'hidden'
-  };
+  if(!notification){
+    return null
+  }
 
-  const errorStyle = {
-    color: "red",
-    background: "lightgrey",
-    fontSize: 20,
-    borderStyle: "solid",
-    borderSadius: 5,
-    padding: 10,
-    marginBottom: 10,
-  };
-
-  console.log('notification: ', notification)
-
-  return (
-    <div
-      style={notificationStyle}
-      className="notification"
-    >
-      {notification}
+  return ( 
+    <div>
+      <Alert severity='info'>{notification}</Alert>
     </div>
   );
 };
