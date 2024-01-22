068b396d0b4fe0234f91caa462302d02dd075cbb
Janne Saari <jannesaari24@gmail.com>
2024-01-22 12:41:08 +0200

    Add allAuthors and Book, finish 8.3

--- part8/library/library-backend.js
+++ part8/library/library-backend.js
@@ -98,16 +98,39 @@ let books = [
 */
 
 const typeDefs = `
+  type Book {
+    title: String!,
+    published: Int,
+    author: String,
+    genres: [String]
+  }
+
+  type Author {
+    name: String!,
+    born: Int,
+    bookCount: Int!
+  }
+
   type Query {
-    bookCount: Int,
-    authorCount: Int
+    bookCount: Int!,
+    authorCount: Int!,
+    allBooks: [Book!]!,
+    allAuthors: [Author!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: (root) => 
+      books.reduce( (count, book ) =>
+        book.author === root.name ? count + 1 : count,
+        0)
   }
 }
 
