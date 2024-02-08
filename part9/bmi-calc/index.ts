5e1696e9c4fff201d8c03f8eef3e909671b8fcd5
Janne Saari <jannesaari24@gmail.com>
2024-02-08 14:46:57 +0200

    fix respond with error if invalid params on /bmi

--- part9/bmi-calc/index.ts
+++ part9/bmi-calc/index.ts
@@ -8,14 +8,28 @@ app.get('/hello', (_req, res) => {
 
 app.get('/bmi', (req, res) => {
   console.log(req.query);
-  const height: number = Number(req.query.height);
-  const weight: number = Number(req.query.weight);
-  const bmi = calculateBmi(height, weight);
-  res.send({
-    height: height,
-    weight: weight,
-    bmi: bmi
-  })
+  try {
+    const height: number = Number(req.query.height);
+    const weight: number = Number(req.query.weight);
+    if(isNaN(height) || isNaN(weight)){
+      throw new Error('Provided values were not numbers!');
+    }
+    const bmi = calculateBmi(height, weight);
+    res.send({
+      height: height,
+      weight: weight,
+      bmi: bmi
+    })
+  } catch (error) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+    res.status(400).send({
+      error: "malformatted parameters"
+    })
+  }
 })
 
 const PORT = 3003;
