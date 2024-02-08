19871ccbb8d5df299df0e78ee42a91ba6d984079
Janne Saari <jannesaari24@gmail.com>
2024-02-08 13:35:28 +0200

    Add express, finish 9.4

--- part9/bmi-calc/bmiCalculator.ts
+++ part9/bmi-calc/bmiCalculator.ts
@@ -31,8 +31,9 @@ const calculateBmi = (height: number, weight: number): string => {
     return 'Normal (healthy weight)'
   else if (bmi >= 25 && bmi <= 29.9)
     return 'Overweight'
-  else if (bmi >= 30)
-    return 'Obese'
+  // else if (bmi >= 30)
+
+  return 'Obese'
 }
 
 try {
