7beb445f0840415bf0733d8f08421877c5f65909
Janne Saari <jannesaari24@gmail.com>
2024-02-13 13:06:09 +0200

    Add /exercises endpoint, finish 9.7

--- part9/bmi-calc/exerciseCalculator.ts
+++ part9/bmi-calc/exerciseCalculator.ts
@@ -1,12 +1,4 @@
-interface Result {
-  periodLength: number,
-  trainingDays: number,
-  success: boolean,
-  rating: number,
-  ratingDescription: string,
-  target: number,
-  average: number
-}
+import { calculateExercises } from "./modules";
 
 interface Exercise {
   target: number,
@@ -24,48 +16,6 @@ const parseExerciseArguments = (args: string[]): Exercise => {
   } else {
     throw new Error('Provided values were not numbers!');
   }
-};
-
-const calculateExercises = (hours: number[], target: number): Result => {
-  const dailyAverage = hours.reduce((hours, day) => 
-    hours + day
-  ) / hours.length;
-  const rateValue = dailyAverage / target;
-  let rating;
-  if(rateValue < 0.5)
-    rating = 1; 
-  else if (rateValue >= 0.5 && rateValue < 1)
-    rating = 2;
-  else 
-    rating = 3;
-
-  let ratingDescription = '';
-  switch (rating) {
-    case 1:
-    ratingDescription = 'could be much better';
-      break;
-    case 2:
-      ratingDescription = 'not too bad but could be better';
-      break;
-    case 3:
-      ratingDescription = 'you hit the goal';
-      break;
-    default:
-      break;
-  }
-
-  return { 
-    periodLength: hours.length,
-    trainingDays: hours.reduce((
-      trainingDays, currentDay) =>
-       currentDay ? trainingDays + 1 : trainingDays 
-       , 0),
-    success: dailyAverage >= target,
-    rating: rating,
-    ratingDescription: ratingDescription,
-    target: target,
-    average: dailyAverage
-  };
 };
 
 try {
--- part9/bmi-calc/index.ts
+++ part9/bmi-calc/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
-const app = express();
+const bmiApp = express();
+const exerciseApp = express();
+exerciseApp.use(express.json());
 import { calculateBmi } from './modules';
+import { calculateExercises } from './modules';
 
-app.get('/hello', (_req, res) => {
+bmiApp.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
-app.get('/bmi', (req, res) => {
+bmiApp.get('/bmi', (req, res) => {
   console.log(req.query);
   try {
     const height: number = Number(req.query.height);
@@ -21,19 +24,59 @@ app.get('/bmi', (req, res) => {
       bmi: bmi
     });
   } catch (error) {
-    let errorMessage = 'Something bad happened.';
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    console.log(errorMessage);
-    res.status(400).send({
-      error: "malformatted parameters"
+      let errorMessage = 'Something bad happened.';
+      if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message;
+      }
+      console.log(errorMessage);
+      res.status(400).send({
+        error: "malformatted parameters"
     });
   }
 });
 
 const PORT = 3003;
 
-app.listen(PORT, () => {
+bmiApp.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+
+exerciseApp.post('/exercises', (req, res) => {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+    const daily_exercises: number[] = req.body.daily_exercises.map((x: number) => {
+      const xAsNumber: number = Number(x);
+      if(isNaN(xAsNumber)){
+        throw new Error('malformatted parameters');
+      }
+      return xAsNumber;
+    });
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const target: number = req.body.target as number;
+    if(isNaN(target)){
+      throw new Error(`malformatted parameters`);
+    }
+    if(target === null || daily_exercises === null){
+      throw new Error('parameters missing!');
+    }
+    const results = calculateExercises(daily_exercises, target);
+    console.log(results);
+    res.json(results);
+  } catch (error) {
+      let errorMessage = '';
+      if (error instanceof Error) {
+        errorMessage = error.message;
+      }
+      console.log(errorMessage);
+      res.status(400).send({
+        error: errorMessage
+      });
+    }
+});
+
+const exercisePort = 3002;
+
+exerciseApp.listen(exercisePort, () => {
+  console.log(`Server running on port ${exercisePort}`);
 });
